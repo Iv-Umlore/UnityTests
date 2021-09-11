d015a0bfff076a0acdec050ffee87f498be80d8e
Iv-Umlore <iv.kiselev1998@yandex.ru>
2021-09-11 17:23:34 +0300

    Changes

---
+++ First Project Unity/Assets/First script.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    [SerializeField] GameObject Platform;
+    [SerializeField] GameObject Platform1;
+    [SerializeField] GameObject Personage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
